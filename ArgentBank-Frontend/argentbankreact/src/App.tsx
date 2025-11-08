1d231eb6b395635d6624fdddf2b806910825fe74
Feihung Mayer <mayerfeihung@gmail.com>
2025-11-08 19:17:47 +0100

    Refactor App component to use named export and improve structure with Router and Routes

--- ArgentBank-Frontend/argentbankreact/src/App.tsx
+++ ArgentBank-Frontend/argentbankreact/src/App.tsx
@@ -1,13 +1,20 @@
-import './App.scss'
+import React from 'react';
+import {Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './pages/home/Home';
+import './App.scss';
 
-const App = () =>  {
+export const App:React.FC = () =>  {
   
 
   return (
     <>
-      
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
     </>
   )
 }
 
-export default App
+
