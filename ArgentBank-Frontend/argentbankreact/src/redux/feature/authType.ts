0c619107fe399acc8c0c13dab0c7e89ad837ab6e
Feihung Mayer <mayerfeihung@gmail.com>
2025-11-15 19:31:00 +0100

    Implement authentication features with Redux: add login, fetch, and update user profile actions and slices

---
+++ ArgentBank-Frontend/argentbankreact/src/redux/feature/authType.ts
@@ -0,0 +1,17 @@
+export interface UserProfile {
+    email: string;
+    firstName: string;
+    lastName: string;
+    userName: string;
+    id: string;
+}
+
+
+export interface AuthState {
+    token: string;
+    loading: boolean;
+    isAuthenticated: boolean;
+    isEditingProfile: boolean;
+    error : string | null;
+    user: userProfile;
+}
---
+++ ArgentBank-Frontend/argentbankreact/src/redux/store.ts
@@ -0,0 +1,35 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import authReducer from "./feature/authSlices";
+
+
+
+
+
+const persistConfig = {
+    key: "auth",
+    storage,
+};
+
+const persistAuthReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+    reducer: {
+        auth: persistAuthReducer,
+    },
+    middleware: (getDefaultMiddleware: (arg0: { serializableCheck: { ignoredActions: string[]; }; }) => any) =>
+        getDefaultMiddleware({
+            serializableCheck: { 
+                ignoredActions: [ "persist/PERSIST" ], //ignore les actions spécifique à Redux Persist
+            },
+        }),
+});
+
+ //Redux Persistore pour synchroniser le store avec le stockage local
+ export const persistor = persistStore(store);
+ //Typage du store
+ export type RootState = ReturnType<typeof store.getState>;
+ export type AppDispatch = typeof store.dispatch;
+ //Exportation du store
+    export default store;
