8dbc3f92bdaa15143d9e74ff7de3c4afbb9311aa
Feihung Mayer <mayerfeihung@gmail.com>
2025-11-22 19:30:01 +0100

    Refactor Navigation component to integrate user authentication; update logout handling and user profile display logic

--- ArgentBank-Frontend/argentbankreact/src/components/navigation/Navigation.tsx
+++ ArgentBank-Frontend/argentbankreact/src/components/navigation/Navigation.tsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/img/argentBankLogo.webp';
+import { logout } from '../../redux/feature/authSlices';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch } from '../../redux/store';
+import { selectIsAuthenticated, selectUserProfile } from '../../redux/feature/authSelector';
+
 
 
  export const Navigation:React.FC = () => {
+      const dispatch: AppDispatch = useDispatch();
+      const isAuthenticated = useSelector(selectIsAuthenticated);
+      const user = useSelector(selectUserProfile);
+      const [userProfileName, setUserProfileName] = useState(''); 
 
-     const [userProfileName, setUserProfileName] = useState(''); // Remplacez par la logique réelle pour obtenir le nom du profil utilisateur
+      const handleLogout = () => {
+        dispatch(logout());
+      };
 
+      useMemo(() => { 
+        if (isAuthenticated) {
+          setUserProfileName( user?.userName || '');
+        }
+       }, [isAuthenticated, user?.userName]);
 
     return (
      <header>
@@ -21,13 +37,13 @@ import logo from '../../assets/img/argentBankLogo.webp';
           <h1 className="sr-only">Argent Bank</h1>
         </NavLink>
         <div>
-          { false ? (
+          { isAuthenticated ? (
             <>
               <NavLink className="main-nav-item" to="/profile">
                 {userProfileName}
                 <i className="fa fa-user-circle"></i>
               </NavLink>
-              <NavLink to="/" className="main-nav-item">
+              <NavLink to="/" className="main-nav-item" onClick ={handleLogout}>
                 <i className="fa fa-sign-out"></i>
                 Sign Out
               </NavLink>
