696332db9aafb538e97817aca9caf8d81a254eb5
Feihung Mayer <mayerfeihung@gmail.com>
2025-11-22 19:30:08 +0100

    Add profile editing functionality to ProfilModif component; integrate Redux dispatch for profile updates

--- ArgentBank-Frontend/argentbankreact/src/components/profilModif/ProfilModif.tsx
+++ ArgentBank-Frontend/argentbankreact/src/components/profilModif/ProfilModif.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import type { AppDispatch } from '../../redux/store';
+import { startProfileEdit } from '../../redux/feature/authSlices';
 
 interface ProfilModifProps {
     firstName: string;
@@ -7,6 +10,12 @@ interface ProfilModifProps {
 
 
  export const ProfilModif:React.FC<ProfilModifProps> = ({firstName, userName}) => {
+    const dispatch: AppDispatch = useDispatch();
+
+    const handleEditClick = () => {
+      dispatch(startProfileEdit());
+    };
+
     return (
         <section>
             <div className="header">
@@ -16,7 +25,7 @@ interface ProfilModifProps {
           {`${firstName} ${userName} `}
           {/* Afficher le nom complet de l'utilisateur */}
         </h1>
-        <button className="edit-button">
+        <button className="edit-button" onClick ={handleEditClick}> 
           Edit Name
         </button>
       </div>
