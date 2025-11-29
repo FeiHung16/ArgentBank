09b48b1ddae54c545e7ccc4e4b05d21f1512d58e
Feihung Mayer <mayerfeihung@gmail.com>
2025-11-29 19:44:53 +0100

    Add login route to App component and organize imports in main.tsx

--- ArgentBank-Frontend/argentbankreact/src/App.tsx
+++ ArgentBank-Frontend/argentbankreact/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/home/Home';
+import { Login } from './pages/login/Login';
 import './App.scss';
+
 
 export const App:React.FC = () =>  {
   
@@ -11,6 +13,7 @@ export const App:React.FC = () =>  {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
     </>
--- ArgentBank-Frontend/argentbankreact/src/main.tsx
+++ ArgentBank-Frontend/argentbankreact/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import {App} from './App.tsx'
-import { Provider } from 'react-redux'
-import store, { persistor } from './redux/store.ts'
-import { PersistGate } from 'redux-persist/integration/react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import {App} from './App.tsx';
+import { Provider } from 'react-redux';
+import store, { persistor } from './redux/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+import "font-awesome/css/font-awesome.min.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
