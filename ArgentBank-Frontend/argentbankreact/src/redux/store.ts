5ba8ffbd26f54a0c07b1ca530382eb56d3349491
Feihung Mayer <mayerfeihung@gmail.com>
2025-12-06 19:58:04 +0100

    Suppression de typage

--- ArgentBank-Frontend/argentbankreact/src/redux/store.ts
+++ ArgentBank-Frontend/argentbankreact/src/redux/store.ts
@@ -31,8 +31,6 @@ export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-// Hooks typés pour utiliser avec TypeScript
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
 //Exportation du store
 export default store;
